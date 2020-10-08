2ba16aa48232e3f91fd5ff55be447b37051cb106
anil-sengul <69522098+anil-sengul@users.noreply.github.com>
2020-10-08 12:24:33 +0200

    Last changes are made for the exam

--- 07_exercise/exercise07.js
+++ 07_exercise/exercise07.js
@@ -1,14 +1,6 @@
 /* 
 Write a function that returns the GCD (greatest common divisor) of 3 integers. 
 
-Example: <br>
-Input: 16, 24, 64 <br>
-Output: 8
-
-Estimated time: 15 minutes <br>
-Total points: 15
-
-*/
 
 //Hello Susanne, my explanations are in the coments.
 
